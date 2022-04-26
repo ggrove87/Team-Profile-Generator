3ca2a7aa1239b8f3dc003803e75cbdc309663591
ggrove87 <gwg1387@gmail.com>
2022-04-26 17:59:47 -0500

    Updated Manager.test.js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,38 @@
 const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    describe("Initialization", () => {
+      it("should set the values of name, id, email, and office number when i set them.", () => {
+        // Arrange
+        let name = "Reniel";
+        let id = 1;
+        let email = "Reniel@gmail.com";
+        let officeNumber = 1;
+        //Act
+        let manager = new Manager(name, id, email, officeNumber);
+  
+        //Assert
+        expect(manager.name).toEqual("Reniel");
+        expect(manager.id).toEqual(1);
+        expect(manager.email).toEqual("Reniel@gmail.com");
+        expect(manager.officeNumber).toEqual(1);
+      });
+    });
+      describe("getRole", () => {
+        it("should return Manager when getRole is called ", () => {
+          //Arrange
+          // Arrange
+          let name = "Reniel";
+          let id = 1;
+          let email = "Reniel@gmail.com";
+          let officeNumber = 1;
+          let manager = new Manager(name, id, email, officeNumber);
+    
+          //Act
+          let role = manager.getRole();
+    
+          //Assert
+          expect(role).toEqual("Manager");
+        });
+      });
+});
